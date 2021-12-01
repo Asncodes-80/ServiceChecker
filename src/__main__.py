41842b1258feef1a357714bc9ae0dc83764f208d
unknown <asn80.asn@hotmail.com>
2021-12-01 15:28:24 +0330

    Add server machine priority assignment with static third machine function

--- src/__main__.py
+++ src/__main__.py
@@ -81,12 +81,37 @@ def server_specification(system_name):
     I can use match like switch case in other lang, but we use python 3.7 and i think
     it would not work.
     """
-    if system_name == "master":
+    if system_name == "master" or system_name == "Master":
         return 0
-    elif system_name == "slave1":
+    elif system_name == "slave1" or system_name == "Slave1":
         return 1
-    elif system_name == "slave2":
+    elif system_name == "slave2" or system_name == "Slave2":
         return 2
+
+
+def define_server_priority(index: int):
+    """Define server priority.
+
+    It's very important to have first, second and third ip address for priority of getting ping
+    and set to db if ping, one of them was 0, that means this computer had leaved network.
+    in other look, to assign the flag to which server? which server is master?
+    This function will return two node index for getting value by index of them, nodes is not important what is slave or what is master
+    but with that we can know which system can be master in the process flow.
+    """
+    p1: int  # System 1 can be master or other
+    p2: int
+
+    if index == 0:
+        p1 = index + 1
+        p2 = p1 + 1
+    elif index == 1:
+        p1 = index - 1
+        p2 = index + 1
+    elif index == 2:
+        p1 = index - 1
+        p2 = p1 - 1
+
+    return {"dev1": p1, "dev2": p2}
 
 
 def slv(ip: List, self_system: str, service_name: str):
@@ -99,6 +124,8 @@ def slv(ip: List, self_system: str, service_name: str):
     related document. if value was 1, this process will jump from to master function activator.
     """
     mongo_client = model.MongoDB(config.main_config["db"]["conn_string"])
+
+    # Check server_specification function for more detail
     system_index = server_specification(self_system)
     shell = wShellCmd.PowerShellCmd()
 
@@ -114,10 +141,13 @@ def slv(ip: List, self_system: str, service_name: str):
 
         time.sleep(config.time_wating)
 
-    master(ip, service_name)
+    # When election is prepared, we will start the service as master of all slaves.
+    master(system_index, ip, service_name, self_system)
 
 
-def master(server_ip: List, service_name: str):
+def master(
+    self_system_index: int, server_ip: List, service_name: str, self_system: str
+):
     """Master activator it long, so quit and read withour stress.
 
     When it will work? and How?
@@ -131,24 +161,68 @@ def master(server_ip: List, service_name: str):
     because result of the Ping was False and will jump into next condition, that perform starting service on slave2, if slave2 wasn't able to
     perform service running major, it will back to loop, util one of the server was able to perform service running.
     """
+    m_client = model.MongoDB(config.main_config["db"]["conn_string"])
     shell = wShellCmd.PowerShellCmd()
 
     while 1:
+        # To getting status of specific service
         local_machine_service_status = shell.run(
             f"Get-Service -name {service_name} | select -First 1 -Expand Status"
         )
 
         if local_machine_service_status == "Stopped":
-            """"""
-            # Dethrone the score of this server and and assign score to another servers
-            # Updating document
-            
+            """What Happend if service status was Stopped?
 
-            # Checking return value of ping other servers
+            It's not problem, it will put her service_status to 0, and will get ping of
+            server slave1 and slave2, if server slave1 or slave2 was available in network
+            priority is first by Server Slave1 then Slave2. if our assume was server 1 master.
+            But in any situation if our master was slave1 (10.99.0.14), what will happen?
+            I implemented this function as define_server_priority if our servers was 3.
+            In the future i will update that with any number of servers.
+            """
+            # Dethrone the score of this server and and assign score to another servers.
+            m_client.update_server_status(
+                server_ip=server_ip[self_system_index], status=0
+            )
 
-            # Priority of running service on which server
+            # Checking return value of ping other servers as slave.
+            # Getting priority function.
+            server_priority = define_server_priority(index=self_system_index)
+            # Ensure for our network computer
+            ping_result_slave1 = shell.get_ping(server_ip[server_priority["dev1"]])
+            ping_result_slave2 = shell.get_ping(server_ip[server_priority["dev2"]])
+
+            # Assign flag to new master.
+            if ping_result_slave1:
+                # Active master feature for this computer.
+                m_client.update_server_status(
+                    server_ip=server_ip[server_priority["dev1"]], status=1
+                )
+                break
+            elif ping_result_slave2:
+                # Active master feature for this computer.
+                m_client.update_server_status(
+                    server_ip=server_ip[server_priority["dev2"]], status=1
+                )
+                break
+            else:
+                # We can log this to system log (Syslog).
+                print(
+                    "request time out for all machines, checking your server network."
+                )
+                break
 
         time.sleep(config.time_wating)
+
+    """ why we need to go back slv activation function?
+    
+    In order to having infinite loop system, that look like a automatic process.
+    If any system Dethrone the score of mastering, we need to new machine to be master.
+    So if master machine goes off from mastering. this machine will be slave, so will be call 
+    slv activation function.
+    """
+
+    slv(server_ip, self_system, service_name)
 
 
 if args.ip_list and args.init_servers == "yes":
