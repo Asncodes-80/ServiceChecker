7b52bab7fc023f03e9ebdffec853fec268dfb2c7
unknown <asn80.asn@hotmail.com>
2021-12-02 19:02:12 +0330

    Separate cli options to from election function, create option config

--- src/__main__.py
+++ src/__main__.py
@@ -1,233 +1,34 @@
 from termcolor import colored
-from datetime import datetime
-from typing import List
-import wShellCmd
 import argparse
 import config
-import model
-import time
+import election_score as election
 
 parser = argparse.ArgumentParser()
+# Process on the list of option in config file
+for option in config.args_parser_options:
+    parser.add_argument(
+        option["name"],
+        option["complete_name"],
+        type=option["type"],
+        help=option["help"],
+    )
 
-# Getting ip list of servers from the pshell
-parser.add_argument(
-    "-ipl",
-    "--ip_list",
-    help="IP Address list is your priority list. [master, slave1, slave2]",
-)
-
-# Getting self machine name (master|slave1|slave2)
-parser.add_argument(
-    "-s",
-    "--self",
-    help="Initialize servers system into collection, only yes or no",
-)
-
-# Getting self machine name (master|slave1|slave2)
-parser.add_argument(
-    "-serv",
-    "--service_name",
-    help="Service name for starting or stopping on the windows ps",
-)
-
-# Getting --init as server document initializer
-parser.add_argument(
-    "-init",
-    "--init_servers",
-    help="Initialize servers system into collection, only yes or no",
-)
 args = parser.parse_args()
 
 
-def init_server(server: List):
-    """This init only for initialize server data on the collection document.
-
-    only one arg, arg is list of server IP, please use proper priority, because it's
-    very important to have first, second and third ip address.
-    first ip will by default master and service_status will 1.
-    ["10.99.0.13", "10.99.0.14", "10.99.0.15"]
-    """
-    ps_cmd = wShellCmd.PowerShellCmd()
-    m_client = model.MongoDB(config.main_config["db"]["conn_string"])
-
-    print(colored("Wait for getting ping, to ensure connection is normal...", "blue"))
-
-    # Getting ping of server list, and put it back in list of ping as ping_result
-    # Will use to having server_status as network status
-    ping_result = []
-    for ip in server:
-        ping = ps_cmd.get_ping(ip)  # return 0 or 1
-        ping_result.append(ping)
-
-    print(colored("Ping completed!", "green"))
-
-    # Main init server in collection by real data.
-    for idx, ip in enumerate(server):
-        init_server = {
-            "ip": ip,
-            "server_status": ping_result[idx],
-            "service_status": 1 if ping_result[idx] == 1 else 0,
-            "date_time": int(datetime.now().timestamp()),
-        }
-        m_client.init_server_document(server_document_initiation=init_server)
-
-
-def server_specification(system_name):
-    """To Specific server name for getting index of that this function will help us.
-
-    Master => 0
-    Slave1 => 1
-    Slave2 => 2
-    I can use match like switch case in other lang, but we use python 3.7 and i think
-    it would not work.
-    """
-    if system_name == "master" or system_name == "Master":
-        return 0
-    elif system_name == "slave1" or system_name == "Slave1":
-        return 1
-    elif system_name == "slave2" or system_name == "Slave2":
-        return 2
-
-
-def define_server_priority(index: int):
-    """Define server priority.
-
-    It's very important to have first, second and third ip address for priority of getting ping
-    and set to db if ping, one of them was 0, that means this computer had leaved network.
-    in other look, to assign the flag to which server? which server is master?
-    This function will return two node index for getting value by index of them, nodes is not important what is slave or what is master
-    but with that we can know which system can be master in the process flow.
-    """
-    p1: int  # System 1 can be master or other
-    p2: int
-
-    if index == 0:
-        p1 = index + 1
-        p2 = p1 + 1
-    elif index == 1:
-        p1 = index - 1
-        p2 = index + 1
-    elif index == 2:
-        p1 = index - 1
-        p2 = p1 - 1
-
-    return {"dev1": p1, "dev2": p2}
-
-
-def slv(ip: List, self_system: str, service_name: str):
-    """Slave activator
-
-    When it will work? and How?
-    All servers in actual mode are slave. in slave function we will check service_status
-    value of this server (we know with IP of that) until their value go to 1 to be master in process.
-    Programme will perform that in several of the time to get 1 from service_status value from
-    related document. if value was 1, this process will jump from to master function activator.
-    """
-    mongo_client = model.MongoDB(config.main_config["db"]["conn_string"])
-
-    # Check server_specification function for more detail
-    system_index = server_specification(self_system)
-    shell = wShellCmd.PowerShellCmd()
-
-    while 1:
-        self_check_service = mongo_client.get_statuses(
-            ip[system_index], "service_status", 1
-        )[0]["service_status"]
-
-        # Checking if election for this is prepared
-        if self_check_service == 1:
-            shell.run(f"start-service {service_name}")
-            break
-
-        time.sleep(config.time_wating)
-
-    # When election is prepared, we will start the service as master of all slaves.
-    master(system_index, ip, service_name, self_system)
-
-
-def master(
-    self_system_index: int, server_ip: List, service_name: str, self_system: str
-):
-    """Master activator it long, so quit and read withour stress.
-
-    When it will work? and How?
-    After process jumped into master activator function, in several time will check status of service
-    if was stopped, it will works hard with this condition:
-    if status of checking_service was stopped, go back to document collection of related server and change
-    value of service_status to 0. that means this server can't work with this service. okay and next, will get
-    ping of slave1 and slave2, if any one of that was not True, it will set in related server document, value 0 into server_status
-    After that will go to our condition, this condition will be a priority of running service on which system. if ping of slave 1 was
-    true will change service_status of slave1 to 1 for continue the service on that server. or if slave1 wasn't able to perform the service
-    because result of the Ping was False and will jump into next condition, that perform starting service on slave2, if slave2 wasn't able to
-    perform service running major, it will back to loop, util one of the server was able to perform service running.
-    """
-    m_client = model.MongoDB(config.main_config["db"]["conn_string"])
-    shell = wShellCmd.PowerShellCmd()
-
-    while 1:
-        # To getting status of specific service
-        local_machine_service_status = shell.run(
-            f"Get-Service -name {service_name} | select -First 1 -Expand Status"
-        )
-
-        if local_machine_service_status == "Stopped":
-            """What Happend if service status was Stopped?
-
-            It's not problem, it will put her service_status to 0, and will get ping of
-            server slave1 and slave2, if server slave1 or slave2 was available in network
-            priority is first by Server Slave1 then Slave2. if our assume was server 1 master.
-            But in any situation if our master was slave1 (10.99.0.14), what will happen?
-            I implemented this function as define_server_priority if our servers was 3.
-            In the future i will update that with any number of servers.
-            """
-            # Dethrone the score of this server and and assign score to another servers.
-            m_client.update_server_status(
-                server_ip=server_ip[self_system_index], status=0
-            )
-
-            # Checking return value of ping other servers as slave.
-            # Getting priority function.
-            server_priority = define_server_priority(index=self_system_index)
-            # Ensure for our network computer
-            ping_result_slave1 = shell.get_ping(server_ip[server_priority["dev1"]])
-            ping_result_slave2 = shell.get_ping(server_ip[server_priority["dev2"]])
-
-            # Assign flag to new master.
-            if ping_result_slave1:
-                # Active master feature for this computer.
-                m_client.update_server_status(
-                    server_ip=server_ip[server_priority["dev1"]], status=1
-                )
-                break
-            elif ping_result_slave2:
-                # Active master feature for this computer.
-                m_client.update_server_status(
-                    server_ip=server_ip[server_priority["dev2"]], status=1
-                )
-                break
-            else:
-                # We can log this to system log (Syslog).
-                print(
-                    "request time out for all machines, checking your server network."
-                )
-                break
-
-        time.sleep(config.time_wating)
-
-    """ why we need to go back slv activation function?
-    
-    In order to having infinite loop system, that look like a automatic process.
-    If any system Dethrone the score of mastering, we need to new machine to be master.
-    So if master machine goes off from mastering. this machine will be slave, so will be call 
-    slv activation function.
-    """
-
-    slv(server_ip, self_system, service_name)
+def convert_ip_list(ipString: str):
+    return ipString.split(" ")
 
 
 if args.ip_list and args.init_servers == "yes":
-    init_server(server=args.ip_list)
+    election.init_server(server=convert_ip_list(args.ip_list))
 elif args.ip_list and args.self and args.service_name and args.init_servers == "no":
-    slv(ip=args.ip_list, self_system=args.self, service_name=args.service_name)
+    election.slv(
+        ip=convert_ip_list(args.ip_list),
+        self_system=args.self,
+        service_name=args.service_name,
+    )
 else:
-    print("Complete the options. you're require to enter server IP list")
+    print(
+        colored("Complete the options. you're require to enter server IP list", "red")
+    )
--- src/config.py
+++ src/config.py
@@ -1,13 +1,41 @@
 """ Config file 
 For using app in any where with diffrent situation and value
 """
+# This is main section of config file for starting the script
+# All option of this script you must enter in terminal to getting correct feed from that.
+args_parser_options = [
+    {
+        "name": "-ipl",
+        "complete_name": "--ip_list",
+        "type": str,
+        "help": "IP Address list is your priority list. [master, slave1, slave2]",
+    },
+    {
+        "name": "-s",
+        "complete_name": "--self",
+        "type": str,
+        "help": "Initialize servers system into collection, only yes or no",
+    },
+    {
+        "name": "-service",
+        "complete_name": "--service_name",
+        "type": str,
+        "help": "Service name for starting or stopping on the windows ps",
+    },
+    {
+        "name": "-init",
+        "complete_name": "--init_servers",
+        "type": str,
+        "help": "Initialize servers system into collection, only yes or no",
+    },
+]
 
 time_wating = 2
 
 main_config = {
     "db": {
-        "conn_string": "mongodb://172.16.24.14:1000/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false",
-        "db_name": "",
-        "collection_name": "",
+        "conn_string": "mongodb://188.213.64.78:1000/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false",
+        "db_name": "Ayandeh_db",
+        "collection_name": "election",
     },
 }
